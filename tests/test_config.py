abe353cbe6cd8c1b1308a97ef5ed13305c3b2a01
Shota Takahashi (KMI) <shotakaha@kmi.nagoya-u.ac.jp>
2023-07-15 11:04:17 +0900

    test(tests/test_config.py): Configクラスのテストモジュールを追加した

---
+++ tests/test_config.py
@@ -0,0 +1,53 @@
+from titanite import config
+
+
+def init_config():
+    settings = {"confd": "../sandbox/", "fname": "config.toml"}
+
+    c = config.Config(**settings)
+    c.init()
+
+    q = c.get("questions").keys()
+    x = c.get("choices").keys()
+
+    expected_q = [
+        "q1",
+        "q2",
+        "q3",
+        "q4",
+        "q5",
+        "q6",
+        "q7",
+        "q8",
+        "q9",
+        "q10",
+        "q11",
+        "q12",
+        "q13",
+        "q14",
+        "q15",
+        "q16",
+        "q17",
+        "q18",
+        "q19",
+        "q20",
+        "q21",
+        "q22",
+    ]
+    expected_x = [
+        "age",
+        "gender",
+        "regional",
+        "subregional",
+        "job_title",
+        "research_group",
+        "research_field",
+        "research_years",
+        "yes_no",
+        "good_poor",
+        "agree_disagree",
+        "school",
+    ]
+
+    assert q == expected_q
+    assert x == expected_x
