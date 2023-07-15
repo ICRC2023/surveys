79c1e1d645ab03bd4f65c13c19f63c68dea23fa7
Shota Takahashi (KMI) <shotakaha@kmi.nagoya-u.ac.jp>
2023-07-15 11:32:10 +0900

    fix(titanite/cli.py): CLIで設定ファイルの内容を確認できるようにした

--- titanite/cli.py
+++ titanite/cli.py
@@ -1,12 +1,23 @@
 import typer
 
+from .config import Config
+
 app = typer.Typer()
 
 
 @app.command()
-def config(item: str):
+def config(fname: str = "config.toml", confd: str = ".", show: bool = False):
     """Show configuration"""
-    print(f"Config item: {item}")
+    settings = {
+        "confd": confd,
+        "fname": fname,
+    }
+    c = Config(**settings)
+    c.load()
+    if show:
+        c.show()
+        return
+    return c
 
 
 @app.command()
