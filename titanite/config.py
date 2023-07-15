98fda81edfdc336fc74223d3121dd735692581d3
Shota Takahashi (KMI) <shotakaha@kmi.nagoya-u.ac.jp>
2023-07-15 11:03:55 +0900

    fix(titanite/config.py): Configクラスを作成した

--- titanite/config.py
+++ titanite/config.py
@@ -1,4 +1,4 @@
-
+import tomllib
 from pathlib import Path
 
 from loguru import logger
@@ -8,14 +8,27 @@ from pydantic import BaseModel
 class Config(BaseModel):
     confd: str = "."
     fname: str = "config.toml"
+    questions: dict | None = {}
+    choices: dict | None = {}
+
+    def init(self):
+        config = self.load_toml()
+        self.questions = config.get("questions")
+        self.choices = config.get("choices")
+
+    def load_toml(self):
+        fname = Path(self.confd) / self.fname
+        with fname.open("rb") as f:
+            config = tomllib.load(f)
+        return config
+
 
 if __name__ == "__main__":
+    settings = {"confd": "../sandbox/", "fname": "config.toml"}
 
-    config_data = {
-        "confd": "John Doe",
-        "fname": "johndoe@example.com"
-    }
-
-    config = Config(**config_data)
-    logger.debug(config.confd)
-    logger.debug(config.fname)
+    c = Config(**settings)
+    c.init()
+    logger.debug(c.confd)
+    logger.debug(c.fname)
+    logger.debug(c.get("questions").keys())
+    logger.debug(c.get("choices").keys())
