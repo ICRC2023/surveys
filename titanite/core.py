620ef13e7901e5525591dbff4e218fddacc111a6
Shota Takahashi (KMI) <shotakaha@kmi.nagoya-u.ac.jp>
2023-07-16 12:30:13 +0900

    fix(titanite/core.py): グループ化する関数を追加した

--- titanite/core.py
+++ titanite/core.py
@@ -1,8 +1,47 @@
+import altair as alt
 import pandas as pd
 
 
-def insight(data: pd.DataFrame, x: str, y: str, z: str = "response") -> pd.DataFrame:
+def group(data: pd.DataFrame, x: str, y: str, z: str = "response") -> pd.DataFrame:
+    """
+    データフレームをグループ化
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        前処理済みのデータフレーム
+    x : str
+        X軸に設定する質問の番号
+    y : str
+        Y軸に設定する質問の番号
+    z : str, optional
+        Z軸に設定する質問の番号, by default "response"
+
+    Returns
+    -------
+    pd.DataFrame
+        グループ化したデータフレーム
+    """
 
     names = [x, y]
     grouped = data.groupby(names)[z].sum().reset_index()
     return grouped
+
+
+if __name__ == "__main__":
+    from loguru import logger
+
+    import titanite as ti
+
+    fname = "../sandbox/config.toml"
+    cfg = ti.Config(fname=fname)
+    cfg.load()
+    category = cfg.categories()
+
+    fname = "../sandbox/tmp_preprocessed.csv"
+    data = pd.read_csv(fname, parse_dates=["timestamp"])
+    data = ti.categorical_data(data, category)
+    logger.debug(len(sorted(data.columns)))
+
+    grouped = group(data, x="q1", y="q2")
+    logger.debug(len(sorted(grouped.columns)))
