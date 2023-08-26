42cd88b82a1e8eee239b281f8262f3be5e499e93
Shota Takahashi (KMI) <shotakaha@kmi.nagoya-u.ac.jp>
2023-08-26 11:17:07 +0900

    docs(docs/conf.py): MySTの設定を追加した

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@
 
 project = 'titanite'
 copyright = '2023, Shota Takahashi'
-author = 'Shota Takahashi'
+author = 'Shota Takahashi / ICRC2023 Diversity Group'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -20,10 +20,32 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
+# -- Options for MyST parser --
+myst_enable_extensions = [
+    "amsmath",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
+
+html_theme_options = {
+    "repository_url": "https://github.com/ICRC2023/diversity",
+    "use_repository_button": True,
+    "use_issues_button": True,
+}
+
+html_title = "ICRC2023 Diversity"
