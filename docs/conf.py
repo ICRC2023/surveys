671fea3a2d36f7fc3d7f49a01686339cd16f564a
Shota Takahashi <shotakaha@kmi.nagoya-u.ac.jp>
2024-04-11 00:33:27 +0900

    fix(docs/conf.py): add extensions

--- docs/conf.py
+++ docs/conf.py
@@ -13,9 +13,7 @@ author = "Shota Takahashi / ICRC2023 Diversity Group"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    "myst_nb",
-]
+extensions = ["myst_nb", "sphinx.ext.autodoc", "sphinx.ext.napoleon"]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
